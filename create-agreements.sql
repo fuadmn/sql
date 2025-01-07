29ace01395a34c8b3c54adb97469f392176d19fa
fuadmn <fuadmohamednor@gmail.com>
2025-01-07 11:32:06 +0300

    TABLES WITH DEFAULT VALUE

---
+++ create-agreements.sql
@@ -0,0 +1,6 @@
+CREATE TABLE agreements (
+    company_name VARCHAR(250),
+    business_name VARCHAR(200),
+    agreements_content TEXT,
+    agreements_data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
---
+++ create-invesment.sql
@@ -0,0 +1,6 @@
+CREATE TABLE agreements (
+    company_name VARCHAR(250),
+    company_address VARCHAR(250),
+    yearly_address INT,
+    isInvesting BOOLEAN DEFAULT FALSE
+);
