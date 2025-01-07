69f01c3f5edf8a8632c153669774954840d5e42d
fuadmn <fuadmohamednor@gmail.com>
2025-01-07 10:58:39 +0300

    HOW TO INSERT DATA INTO A TABLE

---
+++ insert-business-data.sql
@@ -0,0 +1,5 @@
+INSERT INTO business (yearly_revenue, business_name, current_status)
+VALUES(3000, "abc commerce", "not-investment");
+
+INSERT INTO business (yearly_revenue, business_name, current_status)
+VALUES(3000, "XYZ commerce", "investment")
