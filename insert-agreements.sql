9ef49e361452fe6e8151b142ee7e92e46cfe88ea
fuadmn <fuadmohamednor@gmail.com>
2025-01-07 14:35:29 +0300

    INSERTING DATA INTO TABLES

---
+++ insert-agreements.sql
@@ -0,0 +1,3 @@
+
+INSERT INTO agreements (company_name, business_name, agreements_data)
+VALUES("High level Inc", "abc commerce", " simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was  in the 1960s with the release of  sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like  PageMaker including versions of Lorem Ipsum")
---
+++ insert-invesment-data.sql
@@ -0,0 +1,6 @@
+-- INSERT INTO investors (company_name, company_address,  yearly_address, isInvesting)VALUES("High level Inc", "hdd location", 12346, TRUE);
+
+
+INSERT INTO investors (company_name, company_address, yearly_address)VALUES("Super tech Itd", "uii location", 42346);
+
+
